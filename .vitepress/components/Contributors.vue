88fdecfaf750e88235d9468bfc52a07937a7a0d8
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-04-24 06:21:55 +0100

    docs(pt): translate `.vitepress/components/contributors.vue`

--- .vitepress/components/Contributors.vue
+++ .vitepress/components/Contributors.vue
@@ -4,8 +4,8 @@ import { contributors } from '../contributors'
 
 <template>
   <div flex="~ wrap gap2" justify-center>
-    <a v-for="{ name, avatar } of contributors" :key="name" :href="`https://github.com/${name}`" m-0 rel="noopener noreferrer" :aria-label="`${name} on GitHub`">
-      <img loading="lazy" :src="avatar" width="50" height="50" rounded-full h-12 w-12 :alt="`${name}'s avatar`">
+    <a v-for="{ name, avatar } of contributors" :key="name" :href="`https://github.com/${name}`" m-0 rel="noopener noreferrer" :aria-label="`${name} na GitHub`">
+      <img loading="lazy" :src="avatar" width="50" height="50" rounded-full h-12 w-12 :alt="`avatar do ${name}`">
     </a>
   </div>
 </template>
